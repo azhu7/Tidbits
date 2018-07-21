62b785cdf378336c23193d40d8a3cfa5b9a13a56
Alex Zhu <alexzhu@umich.edu>
2018-07-21 13:18:54 -0700

    Refactor with pylint. Non-functional change.

--- project/views.py
+++ project/views.py
@@ -1,23 +1,27 @@
-from flask import render_template, redirect, session, url_for
+"""Views for Tidbits."""
+
+import logging
+
+from flask import redirect, render_template, session, url_for
 
 from project.app import app
 from project.forms import QueryForm
 
-import logging
-
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    """Home page."""
     form = QueryForm()
     if form.validate_on_submit():
         session['query_result'] = form.query.data
-        logging.debug('User queried:|{}|'.format(session['query_result']))
+        logging.debug('User queried:|%s|', session['query_result'])
         return redirect(url_for('.results'))
     return render_template('index.html', title='Search', form=form)
 
 
 @app.route('/results/')
 def results():
+    """Results page."""
     query_result = session.get('query_result', default=None)
     if not query_result:
         logging.error('No query result stored. Navigating to home page.')
