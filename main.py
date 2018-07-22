9f7a16311473a76a33dec53a6f6f34b15a870b87
Alex Zhu <alexzhu@umich.edu>
2018-07-22 00:01:31 -0700

    Fix bug due to App Engine can't write log files

--- main.py
+++ main.py
@@ -9,19 +9,27 @@ from project.app import app
 import project.views  # Make sure Flask loads all endpoints. pylint: disable=unused-import
 
 _PORT = int(os.environ.get('PORT', 5000))
-_LOG_DIR = '.logs/'
-if not os.path.exists(_LOG_DIR):
-    os.makedirs(_LOG_DIR)  # Must create dir as it is used in logging.json.
 
 
-def init_logging(config_path='logging.json', default_level=logging.INFO):
+def init_logging(default_level=logging.INFO):
     """Loads the logging config if present or uses default settings."""
+    if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine/'):
+        # App is running in production environment.
+        config_path = 'logging_prod.json'
+    else:
+        # App is running in dev environment.
+        config_path = 'logging_dev.json'
+        _LOG_DIR = '.logs/'
+        if not os.path.exists(_LOG_DIR):
+            os.makedirs(_LOG_DIR)  # Must create dir as it is used in logging.json.
+
     if os.path.exists(config_path):
         with open(config_path, 'rt') as config_file:
             config = json.load(config_file)
         logging.config.dictConfig(config)
     else:
         logging.basicConfig(level=default_level)
+    logging.info('Successfully initialized logger.')
 
 
 def main():
