4decad355475e5d7c16f63a4b7d0c45c09c021f5
Alex Zhu <alexzhu@umich.edu>
2018-07-27 22:42:40 -0700

    Add scripts for setting env vars

--- project/config.py
+++ project/config.py
@@ -7,3 +7,7 @@ class Config(object):  # pylint: disable=too-few-public-methods
     """Default Flask configuration."""
     DEBUG = True
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'test-key'
+
+    # Load API keys. Value is None if environment variable is not set.
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    TWITTER_API_KEY = os.environ.get('TWITTER_API_KEY')
